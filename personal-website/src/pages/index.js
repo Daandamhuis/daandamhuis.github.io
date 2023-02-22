ce5dcaac0d4c38a6c62b73a1f8362f5496935de2
Daan Damhuis <daan.damhuis@stichd.com>
2023-02-22 20:39:51 +0100

    Reverted CSS to original

--- personal-website/src/pages/index.js
+++ personal-website/src/pages/index.js
@@ -10,8 +10,7 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className="bg-wrapper">
-      <div className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
@@ -23,7 +22,6 @@ function HomepageHeader() {
           </Link>
         </div>
         </div>
-      </div>
     </header>
   );
 }
